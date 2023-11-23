ec7e22018b7d673aff10a7cfa07b7ff211d0750e
LeandroL18 <leandrolipani6@gmail.com>
2023-11-23 14:16:10 +0100

    Botton right completo

---
+++ js/myscript.js
@@ -0,0 +1,28 @@
+// Al click dell’utente sulle frecce (sia Prev che Next), il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
+
+
+const item = document.getElementsByClassName("item");
+console.log(item);
+
+// definizione variabili
+let activeItem = 0;
+
+// selezione bottoni
+const right = document.getElementById("button_right");
+const left = document.getElementById("button_left");
+
+// al click del bottone right spostiamo la classe active all'immagine successiva
+right.addEventListener("click", 
+    function(){
+        if(activeItem < item.length -1){
+            item[activeItem].classList.remove("active");
+
+            activeItem ++;
+
+            item[activeItem].classList.add("active");
+
+        }
+
+    }
+)
+ 
